2da082713739b5ed7fb6e7849bde5e90962ebbb2
Kelly Selden <kellyselden@gmail.com>
2017-12-12 22:56:38 -0800

    rollup

---
+++ tests/unit/broccoli/compile/rewriteImportPath-test.js
@@ -0,0 +1,170 @@
+'use strict';
+
+const path = require('path');
+const expect = require('chai').expect;
+const Compile = require('../../../../lib/broccoli/compile');
+const _relative = Compile.relative;
+const _rewriteImportPath = Compile._rewriteImportPath;
+
+describe.only('Compile - rewriteImportPath', function() {
+  let appAndAddons;
+  let amdModules;
+  let nodeModulesSrc;
+  let projectRoot;
+
+  let appDir;
+  let addonDir;
+
+  let rewriteImportPath;
+
+  beforeEach(function() {
+    appAndAddons = path.resolve('/path/to/appAndAddons');
+    amdModules = path.resolve('/path/to/amdModules');
+    nodeModulesSrc = path.resolve('/path/to/nodeModulesSrc');
+    projectRoot = path.resolve('/path/to/projectRoot');
+
+    appDir = path.join(appAndAddons, 'app-tree-output/my-app');
+    addonDir = path.join(appAndAddons, 'addon-tree-output/my-addon');
+
+    rewriteImportPath = _rewriteImportPath(
+      appAndAddons,
+      amdModules,
+      nodeModulesSrc,
+      projectRoot
+    );
+  });
+
+  function relative(root, id) {
+    return _relative(appAndAddons, path.join(root, id));
+  }
+
+  it('original addon lookup passes through', function() {
+    let id = 'my-addon/addon';
+    let parent = relative(appDir, 'app.js');
+
+    let result = rewriteImportPath(id, parent);
+
+    expect(result).to.equal('my-addon/addon');
+  });
+
+  it('removes .js', function() {
+    let id = relative(addonDir, 'addon.js');
+    let parent = relative(appDir, 'app.js');
+
+    let result = rewriteImportPath(id, parent);
+
+    expect(result).to.equal('my-addon/addon');
+  });
+
+  it('removes index.js', function() {
+    let id = relative(addonDir, 'index.js');
+    let parent = relative(appDir, 'app.js');
+
+    let result = rewriteImportPath(id, parent);
+
+    expect(result).to.equal('my-addon');
+  });
+
+  describe('absolute', function() {
+    it('collides with project config/environment', function() {
+      let id = path.join(projectRoot, 'config/environment.js');
+      let parent = relative(appDir, 'app.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('./config/environment');
+    });
+
+    it('config/environment', function() {
+      let id = path.join(appDir, 'config/environment.js');
+      let parent = relative(appDir, 'app.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('./config/environment');
+    });
+
+    it('node_modules', function() {
+      let id = path.join(nodeModulesSrc, 'lodash/multiply.js');
+      let parent = path.join(nodeModulesSrc, 'lodash/lodash.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('./multiply');
+    });
+  });
+
+  describe('relative', function() {
+    it('app to addon', function() {
+      let id = relative(addonDir, 'addon.js');
+      let parent = relative(appDir, 'app.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('my-addon/addon');
+    });
+
+    it('app to node_modules', function() {
+      let id = relative(nodeModulesSrc, 'lodash/multiply.js');
+      let parent = relative(appDir, 'app.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('lodash/multiply');
+    });
+
+    it('app relative same dir', function() {
+      let id = relative(appDir, 'resolver.js');
+      let parent = relative(appDir, 'app.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('./resolver');
+    });
+
+    it('app relative different dir', function() {
+      let id = relative(appDir, 'resolver.js');
+      let parent = relative(appDir, 'routes/application.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('../resolver');
+    });
+
+    it('addon relative same dir', function() {
+      let id = relative(addonDir, 'addon.js');
+      let parent = relative(addonDir, 'index.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('./addon');
+    });
+
+    it('addon relative different dir', function() {
+      let id = relative(addonDir, 'addon.js');
+      let parent = relative(addonDir, 'lib/index.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('../addon');
+    });
+
+    it('node_modules relative same dir', function() {
+      let id = relative(nodeModulesSrc, 'lodash/multiply.js');
+      let parent = relative(nodeModulesSrc, 'lodash/lodash.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('./multiply');
+    });
+
+    it('node_modules relative different dir', function() {
+      let id = relative(nodeModulesSrc, 'lodash/multiply.js');
+      let parent = relative(nodeModulesSrc, 'lodash/lib/index.js');
+
+      let result = rewriteImportPath(id, parent);
+
+      expect(result).to.equal('../multiply');
+    });
+  });
+});
